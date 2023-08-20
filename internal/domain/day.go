59936ff72c24334ee935b8afc7a46dec027cea3f
Philip Fourie <philip.fourie@theta.co.nz>
2023-08-20 00:48:24 +0000

    Updated comments

--- internal/domain/day.go
+++ internal/domain/day.go
@@ -1,4 +1,4 @@
-// Aggregate root for a day
+// Package domain defines the main constructs for managing daily work schedules.
 package domain
 
 import (
@@ -9,37 +9,38 @@ import (
 )
 
 const (
-	WorkDay       = iota // A workday is a day that a person works
-	NonWorkingDay        // A non-working day is a day that a person does not work
+	// WorkDay indicates a day where work activities occur.
+	WorkDay = iota
+
+	// NonWorkingDay indicates a day free from work activities.
+	NonWorkingDay
 )
 
-// A Day is a day that a person works.
-// It is derived from the person's calendar entries, public holidays and the persons preferred working hours
+// Day represents a specific date where work activities might take place.
+// It includes details like start and end times of work, and is influenced by factors
+// such as a person's calendar entries, public holidays, and preferred working hours.
 type Day struct {
 	ID uuid.UUID
-	// The date of the workday, without the time component
+
+	// Date specifies the calendar date for the work day, excluding the time details.
 	Date time.Time
 
-	// The time the person started work
+	// Start marks the beginning of the work day.
 	Start time.Time
 
-	// The time the person finished work
+	// End signifies the conclusion of the work day.
 	End time.Time
 
-	// A collection of segments that make up the day
-	// It is very possible that a day:
-	// - Has no segments
-	// - Is partially allocated with segments
-	// - Is fully allocated with segments
-	// - Has overlapping segments (for example double bookings)
+	// Segments represents chunks of time within the day. These can be continuous or
+	// can overlap (e.g., due to double bookings). A day may also have no segments.
 	Segments []Segment
 }
 
+// AddSegment attaches a new time segment to the current day.
 func (d *Day) AddSegment(s Segment) error {
-	// Add the segment to the day
-	error := validateSegment(d, s)
-	if error != nil {
-		return error
+	err := validateSegment(d, s)
+	if err != nil {
+		return err
 	}
 
 	d.Segments = append(d.Segments, s)
@@ -47,19 +48,19 @@ func (d *Day) AddSegment(s Segment) error {
 	return nil
 }
 
+// validateSegment ensures the segment adheres to constraints related to start and end times.
 func validateSegment(d *Day, s Segment) error {
-	// - start time is before end time
+	// Ensure segment's start time precedes its end time.
 	if s.Start.After(s.End) {
 		return fmt.Errorf("start time (%s) is after the end time (%s)", s.Start, s.End)
 	}
 
-	// - start time is between start and end of the day
+	// Ensure segment's start time isn't before the day's start time.
 	if s.Start.Before(d.Start) {
-		// return a formatted error
 		return fmt.Errorf("start time (%s) is before the start of the day (%s)", s.Start, d.Start)
 	}
 
-	// - end time is between start and end of the day
+	// Ensure segment's end time doesn't exceed the day's end time.
 	if s.End.After(d.End) {
 		return fmt.Errorf("end time (%s) is after the end of the day (%s)", s.End, d.End)
 	}
@@ -67,25 +68,30 @@ func validateSegment(d *Day, s Segment) error {
 	return nil
 }
 
-// A Segment is a slot in a person's day presenting working and non-working time
+// Segment defines a time interval within a day, representing either working or non-working hours.
 type Segment struct {
 	ID uuid.UUID
 
-	// A description of the segment
+	// Description offers more details about the segment's nature.
 	Description string
 
-	// The time the segment started.
-	// The start time should:
-	// - be before the end time
-	// - between the start and end of the day
+	// Start indicates when the segment begins, and it should fall within the encompassing day's duration.
 	Start time.Time
 
-	// The time the segment ended
-	// The end time should:
-	// - be after the start time
-	// - between the start and end of the day
+	// End indicates when the segment concludes, and it should also stay within the day's limits.
 	End time.Time
 
-	// A flag to indicate if the segment is working or non-working
-	IsWorking bool
+	// IsWorkingTime denotes whether the segment represents productive work time or a break.
+	IsWorkingTime bool
+}
+
+// factory method for creating a new segment
+func NewSegment(description string, start time.Time, end time.Time, isWorkingTime bool) Segment {
+	return Segment{
+		ID:            uuid.New(),
+		Description:   description,
+		Start:         start,
+		End:           end,
+		IsWorkingTime: isWorkingTime,
+	}
 }
