2aea0ba9fc117a64aaaddf15b70971bce84700bc
Philip Fourie <philip.fourie@theta.co.nz>
2023-09-17 19:03:58 +1200

    Validate projects according to naming convention

--- cmd/toggl/project/list.go
+++ cmd/toggl/project/list.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/philipf/gt/internal/togglservices"
 	"github.com/spf13/cobra"
@@ -23,9 +24,39 @@ gt toggl project list
 		items, err := togglservices.GetProjects(nil)
 		cobra.CheckErr(err)
 
-		for _, i := range items {
-			fmt.Printf("%d - %s\n", i.ID, i.Name)
+		validate, err := cmd.Flags().GetBool("validate")
+		cobra.CheckErr(err)
+
+		if !validate {
+			for _, i := range items {
+				fmt.Printf("%d - %s\n", i.ID, i.Name)
+			}
+			return
 		}
+
+		re := regexp.MustCompile(`^\[([^|]+)\|([P|S])\|(\d+)(\|(T\d{8}\.\d+))?\] (.+)$`)
+
+		invalidProjects := make([]togglservices.ToggleProjectElement, 0)
+
+		for _, i := range items {
+			// only print the projects that don't match the naming convention
+			if !re.MatchString(i.Name) {
+				//fmt.Printf("%d - %s\n", i.ID, i.Name)
+				invalidProjects = append(invalidProjects, i)
+			}
+		}
+
+		if len(invalidProjects) == 0 {
+			fmt.Printf("All %d projects match the naming convention\n", len(items))
+			return
+		} else {
+			fmt.Printf("%d project(s) do not match the naming convention:\n", len(invalidProjects))
+
+			for _, i := range invalidProjects {
+				fmt.Printf("%d - %s\n", i.ID, i.Name)
+			}
+		}
+
 	},
 }
 
@@ -33,6 +64,6 @@ func init() {
 	projectsCmd.AddCommand(listCmd)
 
 	// filter
-	//listCmd.Flags().StringP("filter", "f", "", "Filter clients by name")
+	listCmd.Flags().Bool("validate", false, "Validate projects matches the naming convention")
 
 }
