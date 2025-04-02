53a1b6e17dad1e2ce824c80cfc9e3fb462a0bb05
Philip Fourie <philip.fourie@gmail.com>
2025-04-02 19:56:27 +1300

    fix(version): 🐛 update version to 0.11.0 and enhance due date formatting
    * Updated the version output in the `version` command to reflect the new version 0.11.0.
    * Changed the due date format in the `InsertTodo` function to include a calendar emoji for better visibility.

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Version of the gt cli",
 	Run: func(_ *cobra.Command, _ []string) {
-		fmt.Println("gt version 0.10.4")
+		fmt.Println("gt version 0.11.0")
 	},
 }
 
