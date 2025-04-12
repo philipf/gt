1b151f74ee2e04112b6aa0b101821f59595d44b1
Philip Fourie <philip.fourie@gmail.com>
2025-04-12 17:12:42 +1200

    fix(version): 🐛 update version to 0.14.1
    * Incremented the version number in the CLI output to reflect the latest release.

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Version of the gt cli",
 	Run: func(_ *cobra.Command, _ []string) {
-		fmt.Println("gt version 0.14.0")
+		fmt.Println("gt version 0.14.1")
 	},
 }
 
